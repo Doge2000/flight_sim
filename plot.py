9012f6e2ef55d7f82b88f1312232840f0705f3e1
JadeL <JadeL@example.com>
2025-12-11 22:59:07 -0600

    debugged the column line in the py

--- plot.py
+++ plot.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("sim.csv")
 
 plt.figure()
-plt.plot(df["time"], df["alt"], label="Altitude (m)")
+plt.plot(df["Time"], df["Altitude"], label="Altitude (m)")
 plt.xlabel("Time (s)")
 plt.ylabel("Altitude (m)")
 plt.title("Rocket Altitude Over Time")
 plt.show()
 
 plt.figure()
-plt.plot(df["time"], df["v"], label="Velocity (m/s)", color='orange')
+plt.plot(df["Time"], df["Velocity"], label="Velocity (m/s)", color='orange')
 plt.xlabel("Time (s)")
 plt.ylabel("Velocity (m/s)")
 plt.title("Rocket Velocity Over Time")
